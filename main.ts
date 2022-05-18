5a3d2b45546fc0da8a6258ce000b885851d11e67
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-05-18 19:47:47 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+input.onButtonPressed(Button.A, function () {
+    lightGate.calibrate(Deviation.Medium)
+})
+input.onButtonPressed(Button.B, function () {
+    alarm.turnOffAlarmAndBroadcast()
+})
+lightGate.onLightLevelDrop(function () {
+    alarm.turnOnAlarmAndBroadcast()
+})
+radio.setGroup(5)
+basic.forever(function () {
+	
+})
